577d5ae3fba15b0d3fe7d7568cf8eb9d9b2534bb
lucas <romancini12@gmail.com>
2025-12-13 18:36:58 -0300

    att gmail

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const gmailhref = document.getElementById("gmailhref");
+
+
+
+
+function isMobile(){
+    window.matchMedia("(max-width: 768px)").matches;
+}
+
+
+if(isMobile){
+    gmailhref.href = "mailto:contatogabiromancini@gmail.com";
+}
+else{
+    gmailhref.href = "https://mail.google.com/mail/?view=cm&to=contatogabiromancini@gmail.com&su=Contato%20pelo%20site&body=Olá%20vim%20do%20seu%20site%20e%20gostaria%20de%20fazer%20contato%20para%20uma%20parceria";
+}
