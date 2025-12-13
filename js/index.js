4e2e12e8e797ad5ada5daba80ab9856bd2bf5a5d
lucas <romancini12@gmail.com>
2025-12-13 18:45:20 -0300

    13/12

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
-const gmailhref = document.getElementById("gmailhref");
+document.addEventListener("DOMContentLoaded", () =>{
+    const gmailhref = document.getElementById("gmailhref");
+    if(!gmailhref) return;
 
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        gmailhref.href = "mailto:contatogabiromancini@gmail.com";
+    }
+    else{
+        gmailhref.href = "https://mail.google.com/mail/?view=cm&to=contatogabiromancini@gmail.com&su=Contato%20pelo%20site&body=Olá%20vim%20do%20seu%20site%20e%20gostaria%20de%20fazer%20contato%20para%20uma%20parceria";
 
-
-
-function isMobile(){
-    window.matchMedia("(max-width: 768px)").matches;
-}
-
-
-if(isMobile){
-    gmailhref.href = "mailto:contatogabiromancini@gmail.com";
-}
-else{
-    gmailhref.href = "https://mail.google.com/mail/?view=cm&to=contatogabiromancini@gmail.com&su=Contato%20pelo%20site&body=Olá%20vim%20do%20seu%20site%20e%20gostaria%20de%20fazer%20contato%20para%20uma%20parceria";
-}
+    }
+});
