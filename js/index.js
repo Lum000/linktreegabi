7b0ba7335e225285e3d3cebe2b97bf79c26ae345
lucas <romancini12@gmail.com>
2025-12-15 19:50:26 -0300

    alteraçoes de escrita

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", () =>{
     if(!gmailhref) return;
 
     if (window.matchMedia("(max-width: 768px)").matches) {
-        gmailhref.href = "mailto:contatogabiromancini@gmail.com";
+        gmailhref.href = "mailto:contatogabiromancini@gmail.com?subject=Parceria";
     }
     else{
-        gmailhref.href = "https://mail.google.com/mail/?view=cm&to=contatogabiromancini@gmail.com&su=Contato%20pelo%20site&body=Olá%20vim%20do%20seu%20site%20e%20gostaria%20de%20fazer%20contato%20para%20uma%20parceria";
+        gmailhref.href = "https://mail.google.com/mail/?view=cm&to=contatogabiromancini@gmail.com&su=Parceria&body=Olá%20vim%20do%20seu%20site%20e%20gostaria%20de%20fazer%20contato%20para%20uma%20parceria";
 
     }
 });
